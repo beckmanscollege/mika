339b4690ef9b0c737208363687ef09b1474d47d5
Jonas Johansson <jns.johansson@gmail.com>
2023-11-12 19:24:32 +0100

    when button a is pressed send just W

--- mika/CP07/code.py
+++ mika/CP07/code.py
@@ -1,34 +1,30 @@
 import usb_hid
-
 from adafruit_circuitplayground import cp
-from random import randint
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
-
 import board
 import busio
+import time
 
 kbd = Keyboard(usb_hid.devices)
-
-is_touched = False
-
 uart = busio.UART(board.TX, board.RX, baudrate=115200)
 delimiter = ","
 
-# THIS CODE IS FOR CP07, THE WIN MECHANISM
-print("CP07")
+def send_message(char, msg=None):
+    global delimiter
+    msg = str(msg) if msg is not None else ""
+    data = f"{char}{delimiter}{msg}"
+    uart.write(data.encode("utf-8"))
+    print("Sending:", data)
+    time.sleep(0.2)
+
+print("CP07: Win mechanism")
+
+btn_pressed = False
 
 while True:
-    if uart.in_waiting:
-        received_data = uart.readline()
-        decoded_data = received_data.decode("utf-8").strip()
-        parts = decoded_data.split(",")
-
-    if cp.touch_A1:
-        kbd.send(Keycode.SIX)
-        random_number = randint(0, 9)
-        data = "W" + delimiter + str(random_number)
-        print("Sending:", data)
-        uart.write(data.encode("utf-8"))
+    if cp.button_a and not btn_pressed:
+        send_message("W")
+        btn_pressed = True
     else:
-        kbd.release(Keycode.SIX)
+        btn_pressed = False
